7bfb4b2c97c023dd8a83376caa9be5a7a7b77e6e
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2015-10-06 15:37:23 -0500

    Recovered functionality of TerritoryGlmDirector for Colin's Moyamoya paper.

---
+++ src/+mlrois/Contents.m
@@ -0,0 +1,31 @@
+% +MLROIS
+%
+% Files
+%   AbstractRois                - provides default implementations
+%   AcaRois                     - 
+%   BlockedRoisBuilder          - ROIBUILDER creates ROIs, reads from and writes to a filesystem.
+%   CerebellumRois              - 
+%   CsfRois                     - 
+%   DeconvolutionStrategy       - is the interface to deconvolutions in the strategy design pattern
+%   FastRois                    - 
+%   GreyRois                    - 
+%   LeftRois                    - 
+%   LucyRichardsonDeconvolution - is the interface to deconvolutions in the strategy design pattern
+%   ManualRois                  - 
+%   McaRois                     - 
+%   MuellerGartner              - implements J Cereb Blood Flow Metab 1992; 12(4):571--583
+%   PcaRois                     - 
+%   RegularizedDeconvolution    - is the interface to deconvolutions in the strategy design pattern
+%   RightRois                   - RIGHTTROIS   
+%   RoiDirector                 - 
+%   RoiDirectorComponent        - 
+%   RoiDirectorDecorator        - 
+%   RoiDirectorPrevious         - ROIDIRECTOR is a client interface for builders of ROIs
+%   Rois                        - ...
+%   RoisBuilder                 - 
+%   RoisInterface               - 
+%   ThalamusRois                - 
+%   TissuePriorsRois            - 
+%   WatershedRois               - 
+%   WhiteRois                   - 
+%   Wiener2Deconvolution        - WIENER2DECONV is the interface to deconvolutions in the strategy design pattern
